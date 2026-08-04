b33f7ca727a1dc4c95e099faac961f8e2ebbe36f
Gastrash <gastrash501@gmail.com>
2026-08-04 11:32:54 -0300

    footer

---
+++ components/React/layout/FooterSection.jsx
@@ -0,0 +1,29 @@
+//layout/FooterSection.jsx
+
+import FooterComponent from "components/FooterComponent.jsx"
+
+/*
+
+*/
+
+function FooterSection ({
+  layoutSection = "default",
+  dataSection = "1",
+  children
+}) {
+  return (
+    <footer>
+      <div class="footer flex">
+        {children}
+      </div>
+    </footer>
+  )
+}
+
+export default FooterSection
+
+/*
+
+FooterComponent: <small>&copy; 2026 AnimeTV. Todos los derechos reservados.</small>
+
+*/
